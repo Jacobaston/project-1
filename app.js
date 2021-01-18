1d58ac5f6d9b3833a774abfef0abfc4888d5b007
jakeaston <jacobaston92@gmail.com>
2021-01-18 20:37:27 +0000

    Ghost movement and maze update

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const grid = document.querySelector('.grid')
+const points = document.querySelector('#points')
 
 const width = 17 // ! DO NOT TOUCH
 const cells = []
 
 let pacMan = 194
 let ghost1 = 125
-let ghost2 = 126
-let ghost3 = 127
-let ghost4 = 143
+let ghost2 = 128
+let ghost3 = 160
+let ghost4 = 162
+let score = 0
 
 // Create grid
 for (let index = 0; index < width ** 2; index++) {
@@ -20,9 +22,9 @@ for (let index = 0; index < width ** 2; index++) {
 
 const block = Array.from(document.querySelectorAll('div'))
 
-const walls = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 25, 33, 34, 36, 37, 39, 40, 42, 44, 45, 47, 48, 50, 51, 53, 54, 56, 57, 59, 61, 62, 64, 65, 67, 68, 84, 85, 87, 88, 90, 91, 92, 93, 94, 95, 96, 98, 99, 101, 102, 107, 113, 118, 119, 120, 121, 122, 124, 130, 132, 133, 134, 135, 153, 154, 155, 156, 158, 164, 166, 167, 168, 169, 170, 175, 176, 177, 178, 179, 180, 181, 186, 187, 189, 190, 200, 201, 203, 204, 206, 207, 209, 210, 211, 212, 213, 214, 215, 217, 218, 220, 221, 229, 237, 238, 240, 241, 242, 243, 244, 246, 248, 249, 250, 251, 252, 254, 255, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288]
+const walls = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 25, 33, 34, 36, 37, 39, 40, 42, 44, 45, 47, 48, 50, 51, 53, 54, 56, 57, 59, 61, 62, 64, 65, 67, 68, 84, 85, 87, 88, 90, 91, 93, 95, 96, 98, 99, 101, 102, 107, 108, 110, 112, 113, 118, 119, 120, 121, 122, 132, 133, 134, 135, 141, 142, 143, 144, 145, 146, 147, 153, 154, 155, 156, 166, 167, 168, 169, 170, 175, 176, 177, 178, 179, 180, 181, 186, 187, 189, 190, 200, 201, 203, 204, 206, 207, 209, 210, 211, 212, 213, 214, 215, 217, 218, 220, 221, 229, 237, 238, 240, 241, 242, 243, 244, 246, 248, 249, 250, 251, 252, 254, 255, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288]
 
-const food = [18, 19, 20, 21, 22, 23, 24, 26, 27, 28, 29, 30, 31, 32, 35, 38, 41, 43, 46, 49, 55, 58, 60, 63, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 86, 89, 97, 100, 103, 104, 105, 106, 114, 115, 116, 117, 123, 131, 140, 148, 157, 165, 171, 172, 173, 174, 182, 183, 184, 185, 188, 191, 192, 193, 194, 196, 197, 198, 199, 202, 208, 216, 222, 223, 224, 225, 226, 227, 228, 230, 231, 232, 233, 234, 235, 236, 239, 259, 245, 247, 253, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270]
+const food = [18, 19, 20, 21, 22, 23, 24, 26, 27, 28, 29, 30, 31, 32, 35, 38, 41, 43, 46, 49, 55, 58, 60, 63, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 86, 89, 92, 94, 97, 100, 103, 104, 105, 106, 109, 111, 114, 115, 116, 117, 123, 124, 125, 129, 130, 131, 140, 148, 157, 158, 159, 163, 164, 165, 171, 172, 173, 174, 182, 183, 184, 185, 188, 191, 192, 193, 194, 196, 197, 198, 199, 202, 208, 216, 222, 223, 224, 225, 226, 227, 228, 230, 231, 232, 233, 234, 235, 236, 239, 259, 245, 247, 253, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270]
 
 for (let i = 0; i < walls.length; i++) {
   for (let j = 0; j < cells.length; j++) {
@@ -55,27 +57,9 @@ cells[pacMan].classList.remove('pacmanRight')
 pacMan += 1
 cells[pacMan].classList.add('pacmanRight')
 
-// Ghost 1
-cells[ghost1].classList.remove('ghost-1')
-ghost1 += 1
-cells[ghost1].classList.add('ghost-1')
-// Ghost 2
-cells[ghost2].classList.remove('ghost-2')
-ghost2 += 1
-cells[ghost2].classList.add('ghost-2')
-// Ghost 3
-cells[ghost3].classList.remove('ghost-1')
-ghost3 += 1
-cells[ghost3].classList.add('ghost-1')
-// Ghost 4
-cells[ghost4].classList.remove('ghost-1')
-ghost4 += 1
-cells[ghost4].classList.add('ghost-1')
-
 // Movement of character
 document.addEventListener('keyup', (event) => {
   const key = event.keyCode
-  console.log()
 
   if ((key === 68 || key === 39) && !((pacMan + 1) % width === 0) && !(walls.includes(pacMan + 1))) {
     removeFood()
@@ -113,6 +97,75 @@ function removeFood() {
     if (pacMan === food[i]) {
       block[food[i]].classList.remove('food')
       block[food[i]].innerHTML = ''
+      score = score + 10
+      points.innerHTML = score
     }
   }
 }
+
+function getCoordinates(index) {
+  return [index % width, Math.floor(index / width)]
+}
+// Random movement
+function moveGhost(ghost) {
+  const movemennt = [+1, + width, -1, - width]
+  let ghostimerId = ''
+  let direction = movemennt[Math.floor(Math.random() * movemennt.length)]
+
+  if (direction === width) {
+    direction = movemennt[Math.floor(Math.random() * movemennt.length)]
+  } else if (direction === - width) {
+    direction = movemennt[Math.floor(Math.random() * movemennt.length)]
+  } else if (direction === - 1) {
+    direction = movemennt[Math.floor(Math.random() * movemennt.length)]
+  }
+
+  console.log(direction)
+
+  const [ghostX, ghostY] = getCoordinates(ghost)
+  const [pacManX, pacManY] = getCoordinates(pacMan)
+  const [ghostNextX, ghostNextY] = getCoordinates(ghost + direction)
+
+  function isXCoordCloser() {
+    if ((ghostNextX - pacManX) > (ghostX - pacManX)) {
+      return true
+    } else return false
+  }
+
+  function isYCoordCloser() {
+    if ((ghostNextY - pacManY) > (ghostY - pacManY)) {
+      return true
+    } else return false
+  }
+
+  ghostimerId = setInterval(function () {
+    if (!cells[ghost + direction].classList.contains('wall')) {
+      cells[ghost].classList.remove('ghost')
+
+      if (isXCoordCloser() || isYCoordCloser()) {
+        ghost += direction
+        cells[ghost].classList.add('ghost')
+        direction = movemennt[Math.floor(Math.random() * movemennt.length)]
+      } else {
+        cells[ghost].classList.add('ghost')
+        direction = movemennt[Math.floor(Math.random() * movemennt.length)]
+      }
+      cells[ghost].classList.add('ghost')
+    } else direction = movemennt[Math.floor(Math.random() * movemennt.length)]
+
+    if (cells[ghost].classList.contains('pacmanRight')) {
+      alert('Game Over!')
+      clearInterval(ghostimerId)
+      ghost1 = 125
+      ghost2 = 128
+      ghost3 = 160
+      ghost4 = 162
+    }
+
+  }, 200)
+}
+
+moveGhost(ghost1)
+moveGhost(ghost2)
+moveGhost(ghost3)
+moveGhost(ghost4)
